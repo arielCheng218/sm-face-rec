0a7389d2333182f964c47b76241f7c0d0bee8c3c
Ariel <ariel.kerry@gmail.com>
2022-10-25 17:42:17 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#
+# 	This program identifies and tags faces from the St. Mark's students and staff directory. 
+# 	Built for the internal use of the marketing & communications department, for them to photos to SmugMug.
+#
+#		Author: Ariel Cheng, Class of '25
+# 	Date: 10/25/2022
+#
+
+# NOTE: only used CLI to test
+
+# USEFUL LINKS
+# Tolerance value = 0.44
+# Accuracy problems: https://github.com/ageitgey/face_recognition/wiki/Face-Recognition-Accuracy-Problems#question-face-recognition-works-well-with-european-individuals-but-overall-accuracy-is-lower-with-asian-individuals
