eedd80554e2529e78af9c6d1fd5a67593c00d1d7
Ariel <ariel.kerry@gmail.com>
2022-10-25 20:03:31 -0400

    added roadmap

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #
 # 	This program identifies and tags faces from the St. Mark's students and staff directory. 
-# 	Built for the internal use of the marketing & communications department, for them to photos to SmugMug.
+# 	Built for the internal use of the St. Mark's marketing & communications department to auto-tag photos on SmugMug.
 #
 #		Author: Ariel Cheng, Class of '25
 # 	Date: 10/25/2022
 #
 
-# NOTE: only used CLI to test
+# TODO make this work programmatically instead of through CLI
+# TODO use multiple photos as training data see wiki here https://github.com/ageitgey/face_recognition/wiki/Face-Recognition-Accuracy-Problems#question-face-recognition-works-well-with-european-individuals-but-overall-accuracy-is-lower-with-asian-individuals
+# TODO cross-reference no media list
+# TODO test accuracy of model and refine
+# TODO make some form of UI
 
-# USEFUL LINKS
 # Tolerance value = 0.44
-# Accuracy problems: https://github.com/ageitgey/face_recognition/wiki/Face-Recognition-Accuracy-Problems#question-face-recognition-works-well-with-european-individuals-but-overall-accuracy-is-lower-with-asian-individuals
