6315f48029a0ef6ee0d3ea98a5a240abc2bb1758
Ariel <ariel.kerry@gmail.com>
2022-11-03 15:38:34 -0400

    data cleanup

--- main.py
+++ main.py
@@ -6,10 +6,49 @@
 # 	Date: 10/25/2022
 #
 
-# TODO make this work programmatically instead of through CLI
 # TODO use multiple photos as training data see wiki here https://github.com/ageitgey/face_recognition/wiki/Face-Recognition-Accuracy-Problems#question-face-recognition-works-well-with-european-individuals-but-overall-accuracy-is-lower-with-asian-individuals
 # TODO cross-reference no media list
 # TODO test accuracy of model and refine
 # TODO make some form of UI
 
-# Tolerance value = 0.44
+# FIXME duplicate name problem??
+
+import os
+import shutil
+import face_recognition
+
+# clean data
+PATH = "./data/train/"
+
+# constants
+TOLERANCE = 0.45
+
+name_to_count = {}
+
+for directory in os.listdir(PATH):
+	for file_name in os.listdir(f"{PATH}{directory}/"):
+		# Isolate person_name
+		person_name = file_name
+		if "2020" in file_name:
+			first_ind_of_2 = file_name.find("2")
+			person_name = file_name[:first_ind_of_2]
+			if person_name[-1] == "." or person_name[-1] == "_":
+				person_name = person_name[:-1]
+		if "JPG" in file_name:
+			first_ind_of_JPG = person_name.find(".JPG")
+			person_name = person_name[:first_ind_of_JPG]
+		# Make directory for them, if it does not exist
+		if os.path.exists(f"{PATH}{person_name}"):
+			print(f"Path for {person_name} exists.")
+			name_to_count[person_name] += 1
+		else:
+			print(f"Path for {person_name} does not exist. Adding new directory.")
+			os.mkdir(f"{PATH}/{person_name}")
+			name_to_count[person_name] = 1
+		# Rename image
+		old_file_path = f"{PATH}{directory}/{file_name}"
+		new_image_name = f"{PATH}{directory}/" + person_name + str(name_to_count[person_name]) + ".JPG"
+		os.rename(old_file_path, new_image_name)
+		# Move renamed image into directory
+		destination_path = f"{PATH}/{person_name}"
+		shutil.move(new_image_name, destination_path)
